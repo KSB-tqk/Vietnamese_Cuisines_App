7a6f3f218d010413514d8f5b69459040d4db41ef
Nguyễn Thành Hiếu <19521505@gm.uit.edu.vn>
2021-06-07 15:30:58 +0700

    Splash - Part of Login

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_test/screens/splash/body_splash_screens.dart';
+
+class SplashScreen extends StatelessWidget {
+  static String id = 'SplashScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
